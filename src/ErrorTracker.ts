61f1adb224749000f2b9bf7d1deca0aadcf0af6f
Mårten Åsberg <marten.asberg@outlook.com>
2020-07-03 15:48:05 +0200

    Filter out errors that's not "current"
    "Current" meaning errors that aren't caused by what the user is currently writing.

--- src/ErrorTracker.ts
+++ src/ErrorTracker.ts
@@ -1,4 +1,4 @@
-import { DiagnosticChangeEvent, Uri, Diagnostic, window, languages, DiagnosticSeverity, TextEditor } from "vscode";
+import { DiagnosticChangeEvent, Uri, Diagnostic, window, languages, DiagnosticSeverity, Range, TextDocument, Position, TextEditor } from "vscode";
 
 export function OnDidChangeDiagnostics(e: DiagnosticChangeEvent): void {
 	if (window.activeTextEditor !== undefined) {
@@ -15,12 +15,38 @@ export function OnDidChangeDiagnostics(e: DiagnosticChangeEvent): void {
 
 function errorsForActiveFile(editor: TextEditor, errorUris: ReadonlyArray<Uri>): Array<Diagnostic> {
 	if (errorUris.includes(editor.document.uri)) {
-		return errorsForFile(editor.document.uri);
+		return errorsForFile(editor, editor.document.uri);
 	}
 	else {
 		return new Array<Diagnostic>();
 	}
 }
-function errorsForFile(fileUri: Uri): Array<Diagnostic> {
-	return languages.getDiagnostics(fileUri).filter(d => d.severity === DiagnosticSeverity.Error);
+function errorsForFile(editor: TextEditor, fileUri: Uri): Array<Diagnostic> {
+	return languages
+		.getDiagnostics(fileUri)
+		.filter(d => d.severity === DiagnosticSeverity.Error && isErrorCurrent(editor, d));
+}
+
+function textBetween(document: TextDocument, cursor: Position, error: Range): string {
+	if (error.contains(cursor)) {
+		return "";
+	}
+	else {
+		let start: Position;
+		let end: Position;
+		if (cursor.isBeforeOrEqual(error.start)) {
+			start = cursor;
+			end = error.start;
+		}
+		else {
+			start = error.end;
+			end = cursor;
+		}
+
+		return document.getText(new Range(start, end));
+	}
+}
+function isErrorCurrent(editor: TextEditor, error: Diagnostic): boolean {
+	const between = textBetween(editor.document, editor.selection.active, error.range);
+	return /^\s*$/.test(between);
 }
