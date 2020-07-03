6e199e3a6da1d8462a47b507c26f9e340cd40e41
Mårten Åsberg <marten.asberg@outlook.com>
2020-07-03 13:45:17 +0200

    Finds errors (and error messages) in current file

--- src/ErrorTracker.ts
+++ src/ErrorTracker.ts
@@ -1,5 +1,24 @@
-import { DiagnosticChangeEvent } from "vscode";
+import { DiagnosticChangeEvent, Uri, Diagnostic, window, languages, DiagnosticSeverity } from "vscode";
 
 export function OnDidChangeDiagnostics(e: DiagnosticChangeEvent): void {
-	console.log("Error(s) detected!");
+	const errors = errorsForActiveFile(e.uris);
+
+	if (errors.length > 0) {
+		console.log(errors);
+	}
+	else {
+		console.log("No errors");
+	}
+}
+
+function errorsForActiveFile(errorUris: ReadonlyArray<Uri>): Array<Diagnostic> {
+	if (window.activeTextEditor !== undefined && errorUris.includes(window.activeTextEditor.document.uri)) {
+		return errorsForFile(window.activeTextEditor.document.uri);
+	}
+	else {
+		return new Array<Diagnostic>();
+	}
+}
+function errorsForFile(fileUri: Uri): Array<Diagnostic> {
+	return languages.getDiagnostics(fileUri).filter(d => d.severity === DiagnosticSeverity.Error);
 }
