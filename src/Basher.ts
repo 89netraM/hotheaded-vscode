fd214af4a40b11fa4b82c48d2c7946d7d89b2dbd
Mårten Åsberg <marten.asberg@outlook.com>
2020-07-03 18:05:41 +0200

    Bashing with sound files!

--- src/Basher.ts
+++ src/Basher.ts
@@ -1,19 +1,35 @@
-import { window } from "vscode";
+import * as Speaker from "speaker";
+import * as path from "path";
+import * as fs from "fs";
+
+let assetsPath: string;
+export function initBashing(extensionPath: string): void {
+	assetsPath = path.join(extensionPath, "assets");
+}
 
 const bashes: ReadonlyArray<string> = new Array<string>(
-	"What is that?",
-	"What are you writing?",
-	"WTF?",
-	"That's not right!",
-	"That's not even real code!",
-	"Are you dumb?",
-	"That's never gonna work!"
+	"Are you dumb.wav",
+	"That's never gonna work.wav",
+	"That's not even real code.wav",
+	"That's not right.wav",
+	"What are you writing.wav",
+	"What is that.wav",
+	"WTF.wav"
 );
 
 function randomMessage(): string {
 	return bashes[Math.floor(Math.random() * bashes.length)];
 }
+function randomFile(): string {
+	return path.join(assetsPath, randomMessage());
+}
 
 export function bash(): void {
-	window.showInformationMessage(randomMessage());
+	const speaker = new Speaker({
+		channels: 2,
+		bitDepth: 16,
+		sampleRate: 44100
+	});
+
+	fs.createReadStream(randomFile()).pipe(speaker);
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,10 @@
 import { ExtensionContext, languages } from "vscode";
 import { OnDidChangeDiagnostics } from "./ErrorTracker";
+import { initBashing } from "./Basher";
 
 export function activate(context: ExtensionContext) {
+	initBashing(context.asAbsolutePath(""));
+
 	languages.onDidChangeDiagnostics(OnDidChangeDiagnostics, null, context.subscriptions);
 }
 
