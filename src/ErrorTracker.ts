b94c98a245fbb8d56d04a5f849d19e71ae319188
DiegoxK <diego.stv@outlook.es>
2023-06-14 10:42:08 -0500

    formatting fix

--- src/ErrorTracker.ts
+++ src/ErrorTracker.ts
@@ -1,51 +1,32 @@
-import {
-  DiagnosticChangeEvent,
-  Uri,
-  Diagnostic,
-  window,
-  workspace,
-  languages,
-  DiagnosticSeverity,
-  Range,
-  TextDocument,
-  Position,
-  TextEditor,
-} from "vscode";
+import { DiagnosticChangeEvent, Uri, Diagnostic, window, workspace, languages, DiagnosticSeverity, Range, TextDocument, Position, TextEditor } from "vscode";
 import { bash } from "./Basher";
 
 let cooldownTimer: NodeJS.Timeout | null = null;
 
 export function OnDidChangeDiagnostics(e: DiagnosticChangeEvent): void {
-  const activeTextEditor = window.activeTextEditor;
-  if (
-    activeTextEditor &&
-    e.uris.find((uri) => uri.path === activeTextEditor.document.uri.path)
-  ) {
-    const errors = errorsForFile(
-      activeTextEditor,
-      activeTextEditor.document.uri
-    );
-    if (errors.length > 0) {
-      if (errors.filter((d) => !sameAsPrevious(d)).length > 0) {
-        console.log("Error: ", errors);
+	const activeTextEditor = window.activeTextEditor;
+	if (
+		activeTextEditor && e.uris.find(uri => uri.path === activeTextEditor.document.uri.path)
+	) {
+		const errors = errorsForFile(activeTextEditor, activeTextEditor.document.uri);
+		if (errors.length > 0) {
+			if (errors.filter((d) => !sameAsPrevious(d)).length > 0) {
+				console.log("Error: ", errors);
         if (cooldownTimer === null) {
           bash();
           setPreviousError(errors[0]);
-
           const cooldownDuration = getRandomCooldownDuration();
-
           console.log(cooldownDuration);
-
           cooldownTimer = setTimeout(() => {
             cooldownTimer = null;
           }, cooldownDuration);
         }
-      }
-    } else {
-      console.log("No errors");
-      clearPreviousError();
-    }
-  }
+			}
+		} else {
+			console.log("No errors");
+			clearPreviousError();
+		}
+	}
 }
 
 function getRandomCooldownDuration(): number {
@@ -63,54 +44,43 @@ function getRandomCooldownDuration(): number {
 }
 
 function errorsForFile(editor: TextEditor, fileUri: Uri): Array<Diagnostic> {
-  return languages
-    .getDiagnostics(fileUri)
-    .filter(
-      (d) =>
-        d.severity === DiagnosticSeverity.Error && isErrorCurrent(editor, d)
-    );
+	return languages
+		.getDiagnostics(fileUri)
+		.filter(d => d.severity === DiagnosticSeverity.Error && isErrorCurrent(editor, d));
+
 }
 
-function textBetween(
-  document: TextDocument,
-  cursor: Position,
-  error: Range
-): string {
-  if (error.contains(cursor)) {
-    return "";
-  } else {
-    let start: Position;
-    let end: Position;
-    if (cursor.isBeforeOrEqual(error.start)) {
-      start = cursor;
-      end = error.start;
-    } else {
-      start = error.end;
-      end = cursor;
-    }
+function textBetween(document: TextDocument, cursor: Position, error: Range): string {
+	if (error.contains(cursor)) {
+		return "";
+	}
+	else {
+		let start: Position;
+		let end: Position;
+		if (cursor.isBeforeOrEqual(error.start)) {
+			start = cursor;
+			end = error.start;
+		}
+		else {
+			start = error.end;
+			end = cursor;
+		}
 
-    return document.getText(new Range(start, end));
-  }
+		return document.getText(new Range(start, end));
+	}
 }
 function isErrorCurrent(editor: TextEditor, error: Diagnostic): boolean {
-  const between = textBetween(
-    editor.document,
-    editor.selection.active,
-    error.range
-  );
-  return /^\s*$/.test(between);
+	const between = textBetween(editor.document, editor.selection.active, error.range);
+	return /^\s*$/.test(between);
 }
 
 let previousError: Diagnostic | null = null;
 function setPreviousError(error: Diagnostic): void {
-  previousError = error;
+	previousError = error;
 }
 function clearPreviousError(): void {
-  previousError = null;
+	previousError = null;
 }
 function sameAsPrevious(error: Diagnostic): boolean {
-  return (
-    previousError?.code === error.code &&
-    previousError?.range.end.line === error.range.end.line
-  );
+	return previousError?.code === error.code && previousError?.range.end.line === error.range.end.line;
 }
